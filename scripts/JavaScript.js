7db6b325c609503899db535f090a11dc33eb8c08
Anton <harder.anton@gmail.com>
2022-05-08 02:58:23 +0200

    Add project files.

---
+++ scripts/JavaScript.js
@@ -0,0 +1,43 @@
+﻿let worker;
+worker = new Worker("scripts/task1.js");
+
+window.onload = function () {
+
+    document.getElementById("cancel").setAttribute("disabled", true);
+
+
+    // Berechnung starten
+    document.getElementById("compute_pi").onclick = function (event) {
+
+        worker = new Worker("scripts/task1.js");
+        document.getElementById("result").value = "Pi wird berechnet ...";
+        worker.postMessage("start");
+
+        document.getElementById("cancel").removeAttribute("disabled");
+        document.getElementById("compute_pi").setAttribute("disabled", true);
+
+        worker.onmessage = function (event) {
+            console.log(event.data);
+            if (Number.isInteger(event.data)) {
+                document.getElementById("progress_bar").value = event.data;
+                document.getElementById("progress-label").innerHTML = (event.data * 100 / 1000000000) + ' %';                
+            }
+            else {
+                document.getElementById("result").value = event.data;
+                document.getElementById("cancel").setAttribute("disabled", true);
+                document.getElementById("compute_pi").removeAttribute("disabled");
+            }
+        };
+    }
+
+    // Berechnung stoppen
+    document.getElementById("cancel").onclick = function ()
+    {
+        document.getElementById("progress_bar").value = 0;
+        document.getElementById("progress-label").innerHTML = 0 + ' %';
+        document.getElementById("result").value = "Berechnung abgebrochen ...";
+        worker.terminate();
+        document.getElementById("compute_pi").removeAttribute("disabled");
+        document.getElementById("cancel").setAttribute("disabled", true);
+    }
+}
---
+++ scripts/task1.js
@@ -0,0 +1,22 @@
+﻿self.onmessage = function (event) {
+    let result = computePi();
+    
+    self.postMessage(result);
+};
+
+function computePi() {
+    let sum = 0.0;
+    const step = 1e-9;
+
+    let counter = 0;
+
+    for (let i = 0; i < 1_000_000_000; i++) {
+        let x = (i + 0.5) * step;
+        sum = sum + 4.0 / (1.0 + x * x);
+        counter++;
+        if (counter % 10_000_000 == 0) {
+            self.postMessage(counter);
+        }
+    }
+    return sum * step;
+}
